3ec943dbccfcba756e2c40d69bd11feeeec61330
Sahardastani <sahar.dastani4776@gmail.com>
2023-02-18 13:30:56 -0500

    frames

--- frame_extraction.py
+++ frame_extraction.py
@@ -5,8 +5,8 @@ def mkdir_ifnotexists(dir):
         return
     os.mkdir(dir)
 
-vid_file = '/home/sdastani/projects/rrg-ebrahimi/sdastani/flownet2-pytorch/flownet2pytorch/video.mp4'
-frame_pth = '/home/sdastani/projects/rrg-ebrahimi/sdastani/flownet2-pytorch/flownet2pytorch/frames'
+vid_file = '/home/sdastani/projects/rrg-ebrahimi/sdastani/flownet2-pytorch/video.mp4'
+frame_pth = '/home/sdastani/projects/rrg-ebrahimi/sdastani/flownet2-pytorch/frames'
 
 mkdir_ifnotexists(frame_pth)
 cmd = "ffmpeg -i %s -start_number 0 -vsync 0 %s/frame_%%06d.png" % (
